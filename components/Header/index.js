d68804ff56b6a1bf62c3b164d603f138bf831518
Hiter Harris <hiterharris@comcast.net>
2019-12-06 14:50:07 -0500

    removed unused code

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,3 @@
-// STEP 1: Create a header component.
-// -----------------------
-// Using a function create the component you see below:
-//
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
-// And add it to the DOM in the .header-container component
-
 function Header() {
     const container = document.querySelector('.header-container');
 
@@ -27,9 +16,6 @@ function Header() {
     const temp = document.createElement('span');
     temp.classList.add('temp');
     header.appendChild(temp);
-
-
-    return header;
 }
 
 Header();
